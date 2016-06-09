c2e090d47a3ba357f2d82a92d38c468a067e6edd
ggsongnail <343851557@qq.com>
2016-06-09 15:36:38 +0800

    new a Directive appVersion for <span app-version></span>

--- WebContent/app/components/version/version-directive.js
+++ WebContent/app/components/version/version-directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('myApp.version.version-directive', [])
-
+//<span app-version></span>
 .directive('appVersion', ['version', function(version) {
   return function(scope, elm, attrs) {
     elm.text(version);
